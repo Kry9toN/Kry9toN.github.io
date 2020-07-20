1e07b20bc29a56601b0292d2e7bc2f6862ed26f6
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-07-20 22:22:04 +0700

    add button back to up

--- js/main.js
+++ js/main.js
@@ -131,4 +131,25 @@ new WOW().init();
         }
     };
 	}
+	
+	// Header fixed and Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  }
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+  
 });
